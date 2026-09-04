383898f475e9e371f66d4d3640065da40e2e73fc
Lukasz Krawczyk <lukasz.krawczyk@vml.com>
2026-09-04 13:07:33 +0200

    chore: initial config

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pl" className={cn("font-sans", geist.variable)}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="p-8 text-2xl font-bold text-blue-500">
+      Tailwind v4 śmiga!
+    </main>
+  );
+}
